64be4a206dca3d6b93cc4ac73137727e6380b499
wasimdcode <pydwasim@gmail.com>
2024-09-26 15:14:34 +0530

    #

---
+++ Myfirst.java
@@ -0,0 +1,12 @@
+ import java.lang.*;
+
+public class Myfirst
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello, Wasim");
+		System.out.println(arg[0]);
+		System.out.println(arg[1]);
+	}
+
+}
