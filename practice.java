14a67a8e72b03d388eb5a15759fd0d3f69ab4b99
wasimdcode <pydwasim@gmail.com>
2024-10-11 19:17:39 +0530

    #

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+//Here i just practice some function or any other program 
+class practice
+{
+	public static void main(String arg[])
+	{
+		int n=50; 
+		System.out.println(Integer.toOctalString(n)); 
+	}
+}
