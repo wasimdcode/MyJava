c9bd1e55fe111d52dc0a9c0fd6b8c88999111ddb
wasimdcode <pydwasim@gmail.com>
2024-11-06 11:48:51 +0530

    Day 6

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -5,10 +5,11 @@ Operators
  */
 public class BitwiseOperator
 {
-    public static void main(String arg[])
+    public static void main(String[] arg)
     {
         int x = 10, y,z;
         int u = -0b1010;
+        int a = 10, b = 6,and,or,xor,not;
 
         /* This is the Left Shift Bitwise Operator "<<"
         We can use this for multiplying values n*2's power k
@@ -23,12 +24,42 @@ public class BitwiseOperator
         Which is used to convert Negative(-) value into Positive(+)
          */
         u = x>>>1;
+
+        /* This is AND"&" Bitwise Operator in java
+        it acts like it is doing multiply at binary bcz
+        When both numbers are 1 so it give 1 otherwise all
+        of them are 0
+         */
+        and = a&b;
+        /* This is OR"|" Bitwise Operator in java
+        It acts like it is doing Addition at binary bcz
+        When any of one number is 1 so it give 1 and
+        when both of numbers are 0 then it gives 0
+         */
+        or = a|b;
+        /* This is XOR"^" Bitwise Operator in Java
+        It is exclusive OR it works on common ones
+        if one of them are one so it give 1 otherwise
+        both are 0 or 1 then it gives 0
+         */
+        xor = a^b;
+        /* This is NOT"~" Bitwise Operator in Java
+        It is simply makes number negative and ad one to it
+        if you take 10 so it makes them -11.
+         */
+        not = ~a;
         System.out.println("This is the Output of Left shift --> " + y);
         System.out.println("This is the Output of Right shift --> " + z);
         System.out.println("This is the Output of Unsigned Right shift --> " + u);
+        System.out.println("This is the Output of AND --> " + and);
+        System.out.println("This is the Output of OR --> " + or);
+        System.out.println("This is the Output of XOR --> " + xor);
+        System.out.println("This is the Output of NOT --> " + not);
 
-
-        System.out.println(String.format("%s",Integer.toBinaryString(x)));
-        System.out.println(String.format("%32s",Integer.toBinaryString(y)));
+        //This is for binary representation
+        System.out.println("These are the Binary Representation of Numbers");
+        System.out.println("These are the Binary Representation of Numbers");
+        System.out.printf("%s%n",Integer.toBinaryString(x));
+        System.out.printf("%32s%n",Integer.toBinaryString(y));
     }
 }
